215b37ef463f838d339d0f4c55e234ccb004c064
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-11-23 15:10:39 +0500

    Task2

---
+++ Tyuiu.LevakovaAA.Sprint5.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint5.Task2.V23.Lib;
+using System;
+using System.IO;
+namespace Tyuiu.LevakovaAA.Sprint5.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.LevakovaAA.Sprint5.Task2.V23/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LevakovaAA.Sprint5.Task2.V23.Lib;
+namespace Tyuiu.LevakovaAA.Sprint5.Task2.V23
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3] { { 6, 0, 6 }, { -1, -8, 7 }, { 5, 1, 0 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            Console.Title = "Спринт #5 | Выполнила: Левакова А.А. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Класс File. Запись структурированных данных в текстовый файл            *");
+            Console.WriteLine("* Задание 2                                                               *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Левакова А.А. | СМАРТб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов. Заменить           *");
+            Console.WriteLine("* положительные элементы массива на 1, отрицательные на 0. Результат      *");
+            Console.WriteLine("* сохранить в файл OutPutFileTask2.csv и вывести на консоль.              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:");
+
+            for (int i  = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
+
